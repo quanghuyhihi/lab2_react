d05fed593f9d557ed4ab9e3541fe36a85320ca33
quanghuyhihi <huypqph30308@fpt.edu.vn>
2024-03-14 22:03:53 +0700

    first commit

--- src/component/products.tsx
+++ src/component/products.tsx
@@ -1,97 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
-type Props = {}
+const Products = () => {
+    const [products, setProducts] = useState([]);
 
-const Products = (props: Props) => {
-  return (
-    <section className="bg-light">
-    <div className="container py-5">
-        <div className="row text-center py-3">
-            <div className="col-lg-6 m-auto">
-                <h1 className="h1">Featured Product</h1>
-                <p>
-                    Reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident.
-                </p>
+    useEffect(() => {
+
+        fetch(`https://dummyjson.com/products?limit=12`)
+            .then((res) => res.json())
+            .then((data) => {
+                setProducts(data.products);
+            });
+    }, [])
+
+    return (
+        <div className="container my-5">
+            <div className="row">
+                {products.map((product: any,index:number) => {
+                    return (
+                        <div key={index} className="col-3 ">
+                                <img className='w-100 ' src={product.thumbnail} />
+                            <h3 className='text-center'>{product.title}</h3>
+                            <p className='text-center'>${product.price}</p>
+
+                        </div>
+                    )
+                })}
             </div>
         </div>
-        <div className="row">
-            <div className="col-12 col-md-4 mb-4">
-                <div className="card h-100">
-                    <a href="shop-single.html">
-                        <img src="/img/feature_prod_01.jpg" className="card-img-top" alt="..."/>
-                    </a>
-                    <div className="card-body">
-                        <ul className="list-unstyled d-flex justify-content-between">
-                            <li>
-                                <i className="text-warning fa fa-star"></i>
-                                <i className="text-warning fa fa-star"></i>
-                                <i className="text-warning fa fa-star"></i>
-                                <i className="text-muted fa fa-star"></i>
-                                <i className="text-muted fa fa-star"></i>
-                            </li>
-                            <li className="text-muted text-right">$240.00</li>
-                        </ul>
-                        <a href="shop-single.html" className="h2 text-decoration-none text-dark">Gym Weight</a>
-                        <p className="card-text">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt in culpa qui officia deserunt.
-                        </p>
-                        <p className="text-muted">Reviews (24)</p>
-                    </div>
-                </div>
-            </div>
-            <div className="col-12 col-md-4 mb-4">
-                <div className="card h-100">
-                    <a href="shop-single.html">
-                        <img src="/img/feature_prod_02.jpg" className="card-img-top" alt="..."/>
-                    </a>
-                    <div className="card-body">
-                        <ul className="list-unstyled d-flex justify-content-between">
-                            <li>
-                                <i className="text-warning fa fa-star"></i>
-                                <i className="text-warning fa fa-star"></i>
-                                <i className="text-warning fa fa-star"></i>
-                                <i className="text-muted fa fa-star"></i>
-                                <i className="text-muted fa fa-star"></i>
-                            </li>
-                            <li className="text-muted text-right">$480.00</li>
-                        </ul>
-                        <a href="shop-single.html" className="h2 text-decoration-none text-dark">Cloud Nike Shoes</a>
-                        <p className="card-text">
-                            Aenean gravida dignissim finibus. Nullam ipsum diam, posuere vitae pharetra sed, commodo ullamcorper.
-                        </p>
-                        <p className="text-muted">Reviews (48)</p>
-                    </div>
-                </div>
-            </div>
-            <div className="col-12 col-md-4 mb-4">
-                <div className="card h-100">
-                    <a href="shop-single.html">
-                        <img src="/img/feature_prod_03.jpg" className="card-img-top" alt="..."/>
-                    </a>
-                    <div className="card-body">
-                        <ul className="list-unstyled d-flex justify-content-between">
-                            <li>
-                                <i className="text-warning fa fa-star"></i>
-                                <i className="text-warning fa fa-star"></i>
-                                <i className="text-warning fa fa-star"></i>
-                                <i className="text-warning fa fa-star"></i>
-                                <i className="text-warning fa fa-star"></i>
-                            </li>
-                            <li className="text-muted text-right">$360.00</li>
-                        </ul>
-                        <a href="shop-single.html" className="h2 text-decoration-none text-dark">Summer Addides Shoes</a>
-                        <p className="card-text">
-                            Curabitur ac mi sit amet diam luctus porta. Phasellus pulvinar sagittis diam, et scelerisque ipsum lobortis nec.
-                        </p>
-                        <p className="text-muted">Reviews (74)</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-  )
-}
+    );
+};
 
-export default Products
+export default Products;
